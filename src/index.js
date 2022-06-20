ec0f05fc1c1cfae97bd6b93cb2de227d4b0f7cf1
Malene Ulm <msul36749@edu.ucl.dk>
2022-06-20 23:05:11 +0200

    populate bookmarks try 2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 'use strict';
 
 module.exports = {
+  register ({ strapi }) {
+    strapi.service('plugin::users-permissions.user').fetchAuthenticatedUser = (id) => {
+      return strapi
+        .query('plugin::users-permissions.user')
+        .findOne({ where: { id }, populate: ['bookmarks'] })
+    }
+  },
   /**
    * An asynchronous register function that runs before
    * your application is initialized.
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
 
   /**
    * An asynchronous bootstrap function that runs before
